01b7edf832c0ef9986aee6390dde9d628a80ec3e
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-07-14 17:31:08 -0600

    first commit

---
+++ control-flujo/02-if.py
@@ -0,0 +1,12 @@
+edad = 54
+if edad > 65:
+    print("Puedes ver la pelicula con descuento super descuento")
+elif edad > 54:
+    print("Puedes ver la pelicula con descuento")
+elif edad > 17:
+    print("Puedes ver la pelicula")
+# else:
+#     print("No Puedes entrar")
+#     print("Ve a otro lado")
+
+print("Listo")
